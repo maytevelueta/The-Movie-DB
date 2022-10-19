9d9c13e142fa4ef4743a73e8bfe2cfcb21d29c36
Mayte Velueta <108358120+maytevelueta@users.noreply.github.com>
2022-10-19 23:05:46 +0000

    1910

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// Example of api call. Get your api key from https://www.themoviedb.org/ and replace it with the one below. 
+// Example of api call. Get your api key from https://www.themoviedb.org/ and replace it with the one below.
 // See https://developers.themoviedb.org/3/getting-started/introduction for more info
-const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9925d2a8efd45a80c5cad431fc453106&page=1";
+const APIURL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9925d2a8efd45a80c5cad431fc453106&page=2";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=9925d2a8efd45a80c5cad431fc453106&query=";
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?&api_key=9925d2a8efd45a80c5cad431fc453106&query=";
 
 const search = document.getElementById("search");
 const button = document.getElementById("button");
@@ -11,40 +13,44 @@ const main = document.getElementById("main");
 getMovie(APIURL);
 
 async function getMovie(url) {
-   const resp = await fetch(url);
-   const respData = await resp.json();
-   console.log(respData);
-   showMovies(respData.results);
+  const resp = await fetch(url);
+  const respData = await resp.json();
+  console.log(respData);
+  showMovies(respData.results);
 }
 
 function showMovies(movies) {
-   main.innerHTML = '';
-   movies.forEach(movie => {
-      const { poster_path, title, vote_average, overview } = movie;
-      const movieEl = document.createElement('div');
-      movieEl.classList.add('col');
-      movieEl.innerHTML = `
-            <div class="card">
+  main.innerHTML = "";
+  movies.forEach((movie) => {
+    const { poster_path, title, vote_average, overview } = movie;
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
                 <img 
          	        src="${IMGPATH + poster_path}" 
                     alt="${title}"
                     class="card-img-top"/>
                 <div class="card-body">
                     <h3 class="card-title">${title}</h3>
-                    <p class="card-rate">${vote_average}/10</p>
-                    <h3 class="card-title">Overview:</h3>
-                    <p class="card-text">${overview}</p>
+                    <p class="card-rate">${vote_average}/10</p> 
                 </div>
-            </div>`;
-      main.appendChild(movieEl);
-   });
+                <div class="hide">
+                    <h3>Overview:</h3>
+                    ${overview}
+                </div>`;
+    main.appendChild(movieEl);
+  });
 }
 
-form.addEventListener('submit', e => {
-   e.preventDefault();
-   const searchTerm = search.value;
-   if (searchTerm) {
-      getMovie(SEARCHAPI + searchTerm);
-      search.value = '';
-   }
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchValue = search.value;
+  if (searchValue) {
+    getMovie(SEARCHAPI + searchValue);
+    search.value = "";
+  }
 });
+
+function Home() {
+  window.location = "/";
+}
